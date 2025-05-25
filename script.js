8896766980674f87a089b024bd5ac85cbb184b33
Dunocat639YT <francescpasto@gmail.com>
2025-05-26 00:51:59 +0200

    .

---
+++ script.js
@@ -0,0 +1,4 @@
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("titol").classList.add("typewriter");
+  document.getElementById("subtitol").classList.add("typewriter");
+});
